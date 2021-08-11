93bcbcf8fde42d55f113a9d16b9593ac3ff2257d
Hakan9092 <hakanekinci.hb@gmail.com>
2021-08-11 12:53:25 +0200

    Events-example

---
+++ Events-example/script5.js
@@ -0,0 +1,14 @@
+id = "result2"
+onMouseDown = "onMouseDown();"
+onMouseUp = "onMouseUp();" > JavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascriptJavascript
+
+
+    <
+    script >
+    function onMouseDown() {
+        document.getElementById('result2').style.color = "red";
+    }
+
+function onMouseUp() {
+    document.getElementById('result2').style.color = "green";
+}
