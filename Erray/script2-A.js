ef36b07d24c1fcfb006da539fdc2da3eebcfcb3b
Hakan9092 <hakanekinci.hb@gmail.com>
2021-08-12 09:29:21 +0200

    ArrayMetod

---
+++ Erray/script2-A.js
@@ -0,0 +1,21 @@
+var langs = [
+    "PHP",
+    "Asp.Net",
+    "JSP",
+    "NodeJS",
+    "Javascript",
+    "Angular JS",
+    "React JS",
+    "Vue JS",
+    "HTML5",
+    "CSS3"
+];
+
+document.getElementById('result').innerHTML = langs.join("--");
+/*document.getElementById('result').innerHTML = langs.join("-");
+document.getElementById('result').innerHTML = langs.join("///");*/
+
+/*function Add() {
+    langs[langs.length] = "MySQL";
+    document.getElementById('result').innerHTML = langs;
+}*/
