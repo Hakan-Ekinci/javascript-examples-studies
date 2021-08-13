aeab9fe114ce3a3cb2de8a06e5a73de3fc812e57
Hakan9092 <hakanekinci.hb@gmail.com>
2021-08-13 18:38:28 +0200

    number-properties-work

---
+++ NumberProperties/script3-N.js
@@ -0,0 +1,35 @@
+show();
+
+function show() {
+    /*var x;
+    x = new Number(7); * /
+    document.getElementById('result').innerHTML = *
+        /*Number(true) + "<br>" +
+        Number(false) + "<br>" +
+        Number(new Date()) + "<br>" +
+        Number("7") + "<br>" +
+        Number("70") + "<br>" +
+        Number("50") + "<br>" +
+        Number("7 50") + "<br>" +
+        Number("JS") + "<br>";*/
+
+    document.getElementById('result').innerHTML =
+        /*parseInt("7") + "<br>" +
+        parseInt("70") + "<br>" +
+        parseInt("50") + "<br>" +
+        parseInt("50") + "<br>" +
+        parseInt("7 60") + "<br>" +
+        parseInt("JS") + "<br>" +
+        parseInt("7 days") + "<br>" +
+        parseInt("days 28") + "<br>";*/
+        parseFloat("7") + "<br>" +
+        parseFloat("70") + "<br>" +
+        parseFloat("50") + "<br>" +
+        parseFloat("7 60") + "<br>" +
+        parseFloat("JS") + "<br>" +
+        parseFloat("3.14") + "<br>" +
+        parseFloat("7 days") + "<br>" +
+        parseFloat("days 28") + "<br>";
+
+
+}
