87a98b9a8765e1cd1d199bbb6518dad671770019
Hakan9092 <hakanekinci.hb@gmail.com>
2021-08-14 17:11:13 +0200

    math-object-work

---
+++ MathObject/script-mat.js
@@ -0,0 +1,9 @@
+document.getElementById('result').innerHTML =
+    Math.E + "<br>" +
+    Math.PI + "<br>" +
+    Math.SQRT2 + "<br>" +
+    Math.SQRT1_2 + "<br>" +
+    Math.LN2 + "<br>" +
+    Math.LN10 + "<br>" +
+    Math.LOG10E + "<br>" +
+    Math.LOG2E + "<br>";
