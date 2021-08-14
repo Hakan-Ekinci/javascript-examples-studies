32fbf419c7a39c28c9ba184d22a392bc9792c7cd
Hakan9092 <hakanekinci.hb@gmail.com>
2021-08-15 00:26:06 +0200

    Typeof-work

---
+++ Typeof/script-T.js
@@ -0,0 +1,15 @@
+var
+    x, y, z, a, b, person;
+x = 7; //Number
+y = "Javascript" //String
+z = true; //boolean
+b = "" //null string
+person = {
+    firstName: "Hakan",
+    lastName: "Ekinci"
+}; //Objekt
+document.getElementById('result').innerHTML = typeof x;
+/*document.getElementById('result').innerHTML = typeof y;*/
+/*document.getElementById('result').innerHTML = typeof z;*/
+/*document.getElementById('result').innerHTML = typeof b;*/
+/*document.getElementById('result').innerHTML = typeof a;*/
