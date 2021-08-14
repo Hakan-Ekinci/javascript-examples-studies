da12cf0aa6050b4e37e2379d7f2734d136880ef7
Hakan9092 <hakanekinci.hb@gmail.com>
2021-08-15 00:24:31 +0200

    Syntax-Rule-Work

---
+++ syntaxRule/script-S.js
@@ -0,0 +1,13 @@
+var x, y, z;
+x = 70; // x  
+y = 5;
+z = x + y; // function that performs addition
+document.getElementById('result').innerHTML = z;
+
+//It works different from top to bottom.
+
+var firstName, lastName;
+
+firstName = "Hakan";
+lastName = "EKINCI";
+document.getElementById('result').innerHTML = firstName + "" + lastName;
