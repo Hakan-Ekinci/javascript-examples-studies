f854a147f3af8692e6867f042afe0f8366a8961b
Hakan9092 <hakanekinci.hb@gmail.com>
2021-08-23 23:14:59 +0200

    HTM -DOM3-Work

---
+++ HTM DOM3/script.js
@@ -0,0 +1,5 @@
+var elementNodes = document.querySelector('#p8').parentNode;
+elementNodes.style.color = "red";
+elementNodes.style.padding = "40px";
+elementNodes.style.backroundColor = "yellow";
+elementNodes.style.textAlign = "center";
